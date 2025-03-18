802b381e1978e8685f711f17985fbb2b4f04525c
PaulHaze <dev@artemist.com.au>
2025-03-18 16:51:46 +1100

    style change to toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center px-2 pt-10">
-      <h1 className="text-center font-semibold">
-        TW/DaisyUI/ShadCN Next Starter
-      </h1>
+      <h1 className="text-center font-semibold">Dashboard Tutorial</h1>
     </div>
   );
 }
