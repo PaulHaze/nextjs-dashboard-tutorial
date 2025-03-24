b8cbb356c744807032660a6368fc84ed636ded78
PaulHaze <dev@artemist.com.au>
2025-03-25 10:19:47 +1100

    add the hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
 import { AcmeLogo } from '@/components/ui';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 
@@ -26,8 +28,22 @@ export default function HomePage() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-8 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Desktop hero image"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="md:hidden"
+            alt="Mobile hero image"
+          />
         </div>
       </div>
     </main>
