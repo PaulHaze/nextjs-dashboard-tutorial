9a70cbd9d1c93f1ac4e8aa87858a72cf3f1d3f5f
PaulHaze <dev@artemist.com.au>
2025-03-25 17:03:28 +1100

    style fixes to hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,13 +28,13 @@ export default function HomePage() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-8 md:py-12">
+        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-4 md:py-12">
           {/* Add Hero Images Here */}
           <Image
             src="/hero-desktop.png"
             width={1000}
             height={760}
-            className="hidden md:block"
+            className="hidden md:block md:w-[90%] lg:max-w-[700px]"
             alt="Desktop hero image"
           />
           <Image
