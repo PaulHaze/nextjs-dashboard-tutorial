ed8c4e3a00896967abcbe137f997a71e071f4107
PaulHaze <dev@artemist.com.au>
2025-03-25 14:59:22 +1100

    swap <a> from Link

--- src/components/nav/NavLinks.tsx
+++ src/components/nav/NavLinks.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -22,14 +24,14 @@ export function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className="bg-base-100/50 flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
