764f2969a55d15cab02ff36322fc1baeaf55d9ce
PaulHaze <dev@artemist.com.au>
2025-04-28 16:52:37 +1000

    fix sekelteon css issue

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,7 +1,10 @@
+import { CardSkeleton } from '@/components/ui';
+
 export default function LoadingPage() {
   return (
     <div>
       <p>Loading...</p>
+      <CardSkeleton />
     </div>
   );
 }
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export * from './AcmeLogo';
 export * from './Card';
 export * from './RevenueChart';
 export * from './LatestInvoices';
+export * from './Skeletons';
