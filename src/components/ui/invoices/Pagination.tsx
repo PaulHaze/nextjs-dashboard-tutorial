8297ad44b0d9a57facbf7a754c6ffbb437f6073e
PaulHaze <dev@artemist.com.au>
2025-07-17 11:30:50 +1000

    add pagination component

--- src/components/ui/invoices/Pagination.tsx
+++ src/components/ui/invoices/Pagination.tsx
@@ -3,7 +3,8 @@
 import Link from 'next/link';
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 
-import { cn, generatePagination } from '@/lib/utils';
+import { cn } from '@/lib/utils';
+// import { generatePagination } from '@/lib/utils';
 
 // ${pathname === link.href && 'bg-sky-100 text-blue-600'}
 
@@ -31,6 +32,38 @@ export function PaginationNumber({
   ) : (
     <Link href={href} className={className}>
       {page}
+    </Link>
+  );
+}
+
+export function PaginationArrow({
+  href,
+  direction,
+  isDisabled,
+}: {
+  href: string;
+  direction: 'left' | 'right';
+  isDisabled?: boolean;
+}) {
+  const className = cn(
+    'flex h-10 w-10 items-center justify-center rounded-md border',
+    isDisabled && 'pointer-events-none text-gray-300',
+    !isDisabled && 'hover:bg-gray-100',
+    direction === 'left' && 'mr-2 md:mr-4',
+    direction === 'right' && 'ml-2 md:ml-4',
+  );
+  const icon =
+    direction === 'left' ? (
+      <ArrowLeftIcon className="w-4" />
+    ) : (
+      <ArrowRightIcon className="w-4" />
+    );
+
+  return isDisabled ? (
+    <div className={className}>{icon}</div>
+  ) : (
+    <Link className={className} href={href}>
+      {icon}
     </Link>
   );
 }
