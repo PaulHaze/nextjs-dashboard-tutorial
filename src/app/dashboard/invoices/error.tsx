c565ec5a2160603e6d1ddee14baab7345eaabaa0
PaulHaze <dev@artemist.com.au>
2025-07-22 08:49:09 +1000

    add a go back to invoices error

--- src/app/dashboard/invoices/error.tsx
+++ src/app/dashboard/invoices/error.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function ErrorPage({
@@ -17,15 +18,26 @@ export default function ErrorPage({
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h2 className="text-center">Something went wrong!</h2>
-      <button
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
-        onClick={
-          // Attempt to recover by trying to re-render the invoices route
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+      <div className="flex gap-4">
+        <button
+          className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+          onClick={
+            // Attempt to recover by trying to re-render the invoices route
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+        <button
+          className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+          onClick={
+            // Attempt to recover by trying to re-render the invoices route
+            () => redirect('/dashboard/invoices')
+          }
+        >
+          Go Back
+        </button>
+      </div>
     </main>
   );
 }
